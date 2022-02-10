b1cc1bac3635cdd230c63a8bf82c38ec5b35250e
yurcha3777 <ivantsiv0683@gmail.com>
2022-02-10 17:04:18 +0200

    OktenHomeWork

--- HomeWork(JS)(08.02.2022)/ClassWork.js
+++ HomeWork(JS)(08.02.2022)/ClassWork.js
@@ -1,3 +1,82 @@
 // 1. - створити функцію яка приймає три числа та виводить найменьше. (Без Math.min!)
-function number(n1,n2,n3)
+// function numberMin(n1, n2, n3) {
+//     if (n1 < n2 && n1 < n3) {
+//         document.write(n1);
+//         console.log(n1);
+//     } else if (n2 < n3 && n2 > n1) {
+//         document.write(n2);
+//         console.log(n2);
+//     }else {
+//         document.write(n3);
+//         console.log(n3);
+//     }
+// };
+// numberMin(700,350,100)
+
+// 2. - створити функцію яка приймає три числа та виводить найбільше. (Без Math.max!)
+
+// function numberMin(n1, n2, n3) {
+//     if (n3 > n1 && n3 > n1) {
+//         document.write(n3);
+//         console.log(n3);
+//     } else if (n2 < n3 && n2 > n1) {
+//         document.write(n2);
+//         console.log(n2);
+//     }else {
+//         document.write(n1);
+//         console.log(n1);
+//     }
+// };
+// numberMin(700,350,100)
+//
+
+// 3. - створити функцію яка повертає найбільше число з масиву
+// let number = [100, 500, 1000, 3003];
+//
+// function numbernum(arr) {
+//     let max = 0;
+//     for (const el of arr) {
+//         if (max < el) {
+//             max = el;
+//         }
+//     }
+//     return max
+// }
+// let a = numbernum(number)
+// console.log(a)
+
+
+// 4.- створити функцію яка повертає найменьше число з масиву
+
+// let number = [100, 500, -1000, 3003];
+//
+// function numbernum(arr) {
+//     let min = arr[0];
+//     for (const el of arr) {
+//         if (min > el) {
+//             min = el;
+//         }
+//     }
+//     return min
+// }
+// let a = numbernum(number)
+// console.log(a)
+
+// 5.- створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його.
+// Приклад [1,2,10]->13
+
+// let number = [1, 2, 10];
+//
+// function foo(num) {
+//     let res = 0;
+//     for (const numElement of num) {
+//         res = res + numElement;
+//     }
+// return res;
+// }
+//
+// console.log(foo(number));
+//
+
+// 6. - створити функцію яка приймає масив чисел та повертає середнє арифметичне його значень.
 
