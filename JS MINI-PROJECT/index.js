0d0f1946e8e696ba3fe4a05dc97d53e7433f010e
yurcha3777 <ivantsiv0683@gmail.com>
2022-06-06 10:08:16 +0300

    Okten

---
+++ JS MINI-PROJECT/index.js
@@ -0,0 +1,11 @@
+// В index.html
+// 1 получить массив объектов user с endpoint`а https://jsonplaceholder.typicode.com/users
+// 2 Вывести id,name всех user в index.html. Отдельный блок для каждого user.
+// 3 Добавить каждому блоку кнопку/ссылку , при клике на которую происходит переход на страницу user-details.html,
+//  которая имеет детальную информацию про объект на который кликнули
+
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(e => e.json())
+    .then(value => {
+        iter
+    })
